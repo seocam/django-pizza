ed65b6ca80040100c5f4587c04029b56098f8fb3
Sergio Oliveira <sergio@tracy.com.br>
2015-08-18 15:04:36 -0300

    added pizzaria and pizza

---
+++ pizzaria/pizza/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    ingredients = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+    def __repr__(self):
+        return unicode(self)
