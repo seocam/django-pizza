868dd6755ec278425872091699d0b6be391ad74a
Sergio Oliveira <sergio@tracy.com.br>
2015-08-18 15:59:02 -0300

    Added image to pizza

--- pizzaria/pizza/models.py
+++ pizzaria/pizza/models.py
@@ -5,6 +5,7 @@ class Pizza(models.Model):
     name = models.CharField(max_length=128)
     price = models.DecimalField(decimal_places=2, max_digits=5)
     ingredients = models.TextField()
+    picture = models.ImageField(blank=True, null=True)
 
     def __unicode__(self):
         return u'Pizza: {}'.format(self.name)
