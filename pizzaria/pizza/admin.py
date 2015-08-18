65eb8f69089317150b307827824f62c26bfbba4d
Sergio Oliveira <sergio@tracy.com.br>
2015-08-18 15:39:46 -0300

    Custom admin fields

--- pizzaria/pizza/admin.py
+++ pizzaria/pizza/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Pizza
 
 
-admin.site.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'ingredients']
+    list_display = ['name', 'ingredients']
+
+
+admin.site.register(Pizza, PizzaAdmin)
