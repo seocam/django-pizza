d12a21b48c2cfdf0f05635528aeb0334cbeaf3f5
Sergio Oliveira <sergio@tracy.com.br>
2015-08-18 10:50:09 -0300

    Added loops.py

---
+++ pyexamples/loops.py
@@ -0,0 +1,14 @@
+pedidos = [
+    {
+        'nome': 'Mario',
+        'sabor': 'portuguesa'
+    },
+    {
+        'nome': 'Marco',
+        'sabor': 'presunto'
+    }
+]
+
+for pedido in pedidos:
+    print(pedido['nome'], pedido['sabor'])
+
