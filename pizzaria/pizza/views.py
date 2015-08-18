232ad28d684bb5e9bbbccd9ec6ba690cce2048ed
Sergio Oliveira <sergio@tracy.com.br>
2015-08-18 16:56:34 -0300

    List pizzas on menu page

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Pizza
+
 
 def menu(request):
-    return HttpResponse('Hello World')
+    pizzas = Pizza.objects.all()
+    result = ''
+    for pizza in pizzas:
+        result += pizza.name
+        result += '<br>'
+    return HttpResponse(result)
