433c53eee08261c100d76c5e6af150e86070400b
Sergio Oliveira <sergio@tracy.com.br>
2015-08-18 09:44:51 -0300

    fixed typo

--- pyexamples/conditions.py
+++ pyexamples/conditions.py
@@ -1,14 +1,14 @@
 
 print('Hello World')
 
-salse = 'tomato'
+sauce = 'tomato'
 
 order_description = """
 The client asked for a pizza with less cheese.
 Instead of cheese he would like to have more ham.
 """
 
-if salse.startswith('toma'):
+if sauce.startswith('toma'):
     print("it's probably tomato...")
 else:
     print("not tomato")
